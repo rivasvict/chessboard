b9fa0c0304d1adb1f1564eeebf9cacebb81e0891
Victor Rivas <vctr90w@gmail.com>
2015-01-18 13:11:02 -0430

    push for Hoce :3

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var cfg = {
+	position:{
+		a1:'wR',
+		b1:'wQ',
+		c1:'wK',
+		d1:'wR',
+		a2:'wP',
+		b2:'wP',
+		c2:'wP',
+		d2:'wP',
+		a6:'bR',
+		b6:'bQ',
+		c6:'bK',
+		d6:'bR',
+		a5:'bP',
+		b5:'bP',
+		c5:'bP',
+		d5:'bP'
+	},
+	dropOffBoard:'snapback',
+	draggable:true,
+}
+var board = new ChessBoard('board',cfg);
